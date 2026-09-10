eb128bd7ba47c72807dd50ae06d40411be42801e
Jose Reyes <jose_reyes@outlook.de>
2026-09-10 14:38:11 +0200

    Vercel Blob installation

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,6 +10,8 @@ import { Media } from './collections/Media'
 
 import { Projects } from './collections/Projects'
 
+import vercelBlobStorage from '@payloadcms/storage-vercel-blob'
+
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
@@ -31,4 +33,13 @@ export default buildConfig({
   }),
   sharp,
   plugins: [],
+
+  plugins: [
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
+  ],
 })
