85592f56244172c66a913d217185ae9bec2fe413
Jose Reyes <jose_reyes@outlook.de>
2026-09-11 09:47:32 +0200

    Vercel add remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,7 @@ const dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
   images: {
-    localPatterns: [
-      {
-        pathname: '/api/media/file/**',
-      },
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: '*.public.blob.vercel-storage.com' }],
   },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
